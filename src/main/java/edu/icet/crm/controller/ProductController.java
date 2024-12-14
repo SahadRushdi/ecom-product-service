e0f90554d2ab11f74a0f9e1cb2c1045adaa8b1b9
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-12-14 11:35:13 +0530

    Added  Slf4j library for monitoring requests.

--- src/main/java/edu/icet/crm/controller/ProductController.java
+++ src/main/java/edu/icet/crm/controller/ProductController.java
@@ -3,6 +3,7 @@ package edu.icet.crm.controller;
 import edu.icet.crm.model.Product;
 import edu.icet.crm.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,14 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/product")
+@Slf4j
 public class ProductController {
 
     private final ProductService productService;
 
     @GetMapping("/all")
     List<Product> allProducts() {
-        System.out.println("Requesting all products");
+        log.info("Request received.");
         return productService.allProduct();
     }
 
